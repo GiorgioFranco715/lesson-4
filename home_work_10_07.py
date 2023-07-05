a0a4d91dd640795dab3d7df397178d4e9f8e8de9
GiorgioFranco715 <138186314+GiorgioFranco715@users.noreply.github.com>
2023-07-05 13:17:37 +0300

    home_work_10_07

---
+++ home_work_10_07.py
@@ -0,0 +1,62 @@
+
+
+#Завдання 1(Виконано)
+#Користувач вводить із клавіатури три цифри. Необхідно створити число, що містить ці цифри.
+# Наприклад, якщо з клавіатури введено 1, 5, 7, тоді потрібно сформувати число 157.
+
+# numberOne = input("Введіть число: ")
+# numberTwo = input("Введіть число: ")
+# numberThree = input("Введіть число: ")
+# print(numberOne + numberTwo + numberThree)
+
+# Завдання 2(Виконано)
+# Користувач вводить із клавіатури число, що складається з чотирьох цифр.
+# Потрібно знайти добуток цифр. Наприклад, якщо з клавіатури введено 1324, тоді результат добутку 1*3*2*4 = 24.
+
+# numberOne = int(input("Введіть число: "))
+# numberTwo = int(input("Введіть число: "))
+# numberThree = int(input("Введіть число: "))
+# numberFour = int(input("Введіть число: "))
+# print(numberOne * numberTwo * numberThree * numberFour)
+
+# Завдання 3(Виконано)
+# Користувач вводить з клавіатури кількість метрів.
+# Потрібно вивести результат переведення метрів у сантиметри, дециметри, міліметри, милі.
+
+# distance_In_meters = int(input("Введіть число: "))
+# distance_In_centimeters = distance_In_meters * 100
+# distance_In_decimeters = distance_In_meters // 10
+# distance_In_millimeters = distance_In_meters * 1000
+# distance_In_miles = distance_In_meters * 0.00062137
+#
+# print(("В метрах:"),(str(distance_In_meters)))
+# print(("В сантиметрах:"),(str(distance_In_centimeters)))
+# print(("В дециметрах:"),(str(distance_In_decimeters)))
+# print(("В міліметрах:"),(str(distance_In_millimeters)))
+# print(("В милях:"),(str(distance_In_miles)))
+
+# Завдання 4(Виконано)
+# Напишіть програму, що обчислює площу трикутника.
+# Користувач із клавіатури вводить розмір основи трикутника і розмір висоти.
+
+
+# the_Base_of_The_triangle = int(input("Введіть основу трикутника: "))
+# height_Of_the_Triangle = int(input("Введіть висоту трикутника: "))
+# area_Of_triangle = the_Base_of_The_triangle * height_Of_the_Triangle * 1//2
+#
+#
+# print(area_Of_triangle)
+
+
+# Завдання 5 (Поки що невірно виконане-бракує навиків роботи з функцією reverse,підівчусь і трошки пізніше зроблю правильно)
+# Користувач із клавіатури вводить чотиризначне число.
+# Необхідно перевернути число і відобразити результат. Наприклад, якщо введено 4512, результат — 2154.
+
+#user_Numeric = int(input("Введіть число: "))
+#reversed(user_Numeric)
+#user_Numeric.reverse()
+
+#print(user_Numeric)
+
+
+
---
+++ home_work_13_07.py
@@ -0,0 +1,42 @@
+#print("zdorov")
+
+# Завдання 1(Виконано)
+# Користувач вводить із клавіатури три числа.
+# Необхідно знайти суму чисел, добуток чисел. Результат обчислень вивести на екран.
+
+
+# numberOne = int(input("Введіть число: "))
+# numberTwo = int(input("Введіть число: "))
+# numberThree = int(input("Введіть число: "))
+#
+# sum_Numbers = numberOne + numberTwo + numberThree
+# product_Numbers = numberOne * numberTwo * numberThree
+#
+# print(("Сума чисел: "),(sum_Numbers))
+# print(("Добуток чисел: "),(product_Numbers))
+
+# Завдання 2(Виконано)
+# Користувач вводить із клавіатури три числа.
+# Перше число — зарплата за місяць, друге число — сума місячного платежу за кредитом у банку,
+# третє число — заборгованість за комунальні послуги.
+# Необхідно вивести на екран суму, яка залишиться у користувача після всіх виплат
+
+# salary_Received = int(input("Введіть число: "))
+# monthly_Loan_payment = int(input("Введіть число: "))
+# payment_Of_utility_Services = int(input("Введіть число: "))
+#
+# remainder = salary_Received - monthly_Loan_payment - payment_Of_utility_Services
+#
+# print(("Залишок після всіх виплат: "),(remainder))
+
+
+# Завдання 3(Виконано)
+# Напишіть програму, що обчислює площу ромба.
+# Користувач вводить із клавіатури довжину двох його діагоналей.
+
+# diagonalOne = int(input("Введіть число: "))
+# diagonalTwo = int(input("Введіть число: "))
+# area_Of_a_Rhombus = diagonalOne * diagonalTwo * 1//2
+#
+# print(("Площа ромба: "),(area_Of_a_Rhombus))
+
